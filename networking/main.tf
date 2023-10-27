de465b5549b33072506fcc5aa3f9364c71ad6eb8
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-10-27 10:13:14 -0400

    don't need empty blocks now apparently

--- networking/main.tf
+++ networking/main.tf
@@ -17,8 +17,6 @@ terraform {
   }
 }
 
-provider "doormat" {}
-
 data "doormat_aws_credentials" "creds" {
   provider = doormat
   role_arn = "arn:aws:iam::365006510262:role/tfc-doormat-role_multi-product-integration-demo"
